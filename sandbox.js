8a862b57ecf3dc3e09d678ff88046b1de727f843
deepizel <rocmancave@gmail.com>
2022-05-20 10:49:36 +0100

    update

--- sandbox.js
+++ sandbox.js
@@ -1,4 +1,9 @@
 const datestamp = document.querySelector('.timein');
+const form = document.querySelector('inputform');
+
+form.addEventListener('on submit', e => {
+    e.preventDefault();
+});
 
 // date functionality
 const now = new Date();
@@ -9,5 +14,12 @@ const priceStamp = document.querySelector('.autopricing');
 const priceSelect = document.querySelector(".slotno input");
 
 function pricing(){
-    if(priceSelect.value.length =)
+    if (priceSelect.value.length <= 10) {
+        console.log('small car');
+    }
 }
+
+priceSelect.addEventListener('onChange', e => {
+    e.preventDefault();
+    pricing();
+});
