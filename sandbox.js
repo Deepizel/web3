77e81146e198aa8b5061a988a2361a7bf39fbe66
deepizel <rocmancave@gmail.com>
2022-05-20 10:42:13 +0100

    update

--- sandbox.js
+++ sandbox.js
@@ -3,3 +3,11 @@ const datestamp = document.querySelector('.timein');
 // date functionality
 const now = new Date();
 datestamp.innerText = now.toDateString();
+
+//pricing
+const priceStamp = document.querySelector('.autopricing');
+const priceSelect = document.querySelector(".slotno input");
+
+function pricing(){
+    if(priceSelect.value.length =)
+}
